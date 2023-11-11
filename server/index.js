57de7c3fe526b8d29157317db684b71628f0606a
Saketh Ganji <ganjisaketh@gmail.com>
2023-11-11 06:23:40 -0600

    completed register/login

--- server/index.js
+++ server/index.js
@@ -1,74 +1,76 @@
 const express = require('express');
-const app = express();
 const bcrypt = require('bcrypt');
 require('dotenv').config({path:'../.env'});
 const User = require('./models/userClass');
 const mongoose = require("mongoose");
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const url = process.env.MONGO_URL;
 
+const app = express();
 app.use(express.json());
 
-const client = new MongoClient(url, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
+mongoose.connect(process.env.MONGO_URL)
 
-
-async function runMongoOperation(operation) {
-    try {
-        await client.connect();
-        return await operation(client);
-    } catch (error) {
-        console.error('MongoDB operation failed:', error);
-    } finally {
-        await client.close();
-    }
-}
-
-app.post('/register', async (req, res) => {
+// Input Validation Middleware
+const validateRegistration = (req, res, next) => {
     const { email, username, password, repassword } = req.body;
 
-    // Basic validation
-    if (!/@./.test(email) || !/../.test(email))
-        return res.status(400).send("Invalid Email");
-    if (password !== repassword)
-        return res.status(400).send("Passwords do not match");
-    if (password.length < 8)
-        return res.status(400).send("Minimum password length of 8");
-    if (!/\d/.test(password))
-        return res.status(400).send("Password requires at least one number");
+    const emailRegex = /\S+@\S+\.\S+/;
+    if (!emailRegex.test(email)) {
+        return res.status(400).json({ error: "Invalid Email" });
+    }
+    if (password !== repassword) {
+        return res.status(400).json({ error: "Passwords do not match" });
+    }
+    if (password.length < 8) {
+        return res.status(400).json({ error: "Minimum password length of 8" });
+    }
+    if (!/\d/.test(password)) {
+        return res.status(400).json({ error: "Password requires at least one number" });
+    }
+    next();
+};
+
+//Register Route
+app.post('/register', validateRegistration, async (req, res) => {
+    const { email, username, password } = req.body;
+
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+        return res.status(400).json({ error: "Email already in use" });
+    }
+
+    const existingUserByUsername = await User.findOne({ username: username });
+    if (existingUserByUsername) {
+        return res.status(400).json({ error: "Username already in use" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ email, username, password: hashedPassword });
 
     try {
-        await client.connect();
-        const db = client.db(); // Add your DB name here
-        const users = db.collection('users'); // Change 'users' to your collection name if different
-
-        // Check if email already exists
-        const emailExists = await users.findOne({ email });
-        if (emailExists) {
-            await client.close();
-            return res.status(400).send("Email already in use");
-        }
-
-        // Hash the password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        // Insert the new user
-        await users.insertOne({ email, username, password: hashedPassword });
-        await client.close();
-
-        res.status(201).send("User created successfully");
+        await user.save();
+        res.status(201).json({ message: "User registered successfully" });
     } catch (error) {
-        console.error(error);
-        await client.close();
-        res.status(500).send("Error in saving user");
+        res.status(500).json({ error: "Error registering user" });
     }
 });
+
+// Login Route
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email: email });
+    if (!user) {
+        return res.status(400).send("User not found");
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+        return res.status(400).send("Invalid password");
+    }
+
+    res.send("Login successful");
+});
+
 
 // Test route
 app.get('/test', (req, res) => {
@@ -77,4 +79,4 @@ app.get('/test', (req, res) => {
 
 // Listening on port 5000
 const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port localhost:${PORT}`));
