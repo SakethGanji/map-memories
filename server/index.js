d587f0e31ae32a909bf0ebd5e3d087e7d2ff468a
Scuti152 <86808194+Scuti152@users.noreply.github.com>
2023-11-10 23:21:26 -0600

    index

--- server/index.js
+++ server/index.js
@@ -1,20 +1,73 @@
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
+require('dotenv').config({path:'../.env'});
+const User = require('./models/userClass');
+const mongoose = require("mongoose");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const url = process.env.MONGO_URL;
 
 app.use(express.json());
-app.post('/register', (req, res) => {
+
+const client = new MongoClient(url, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+async function runMongoOperation(operation) {
+    try {
+        await client.connect();
+        return await operation(client);
+    } catch (error) {
+        console.error('MongoDB operation failed:', error);
+    } finally {
+        await client.close();
+    }
+}
+
+app.post('/register', async (req, res) => {
     const { email, username, password, repassword } = req.body;
-    // Check email and username unique
-    if(!/@./.test(email) || !/\../.test(email))
-        res.send("Invalid Email");
-    else if(password !== repassword)
-        res.send("Passwords do not match");
-    else if(password.length < 8)
-        res.send("Minimum password length of 8")
-    else if(!/\d/.test(password))
-        res.send("Password requires at least one number")
-    else
-        res.send(`User: ${username} Email: ${email} Password: ${password} Repassword: ${repassword}`);
+
+    // Basic validation
+    if (!/@./.test(email) || !/../.test(email))
+        return res.status(400).send("Invalid Email");
+    if (password !== repassword)
+        return res.status(400).send("Passwords do not match");
+    if (password.length < 8)
+        return res.status(400).send("Minimum password length of 8");
+    if (!/\d/.test(password))
+        return res.status(400).send("Password requires at least one number");
+
+    try {
+        await client.connect();
+        const db = client.db(); // Add your DB name here
+        const users = db.collection('users'); // Change 'users' to your collection name if different
+
+        // Check if email already exists
+        const emailExists = await users.findOne({ email });
+        if (emailExists) {
+            await client.close();
+            return res.status(400).send("Email already in use");
+        }
+
+        // Hash the password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // Insert the new user
+        await users.insertOne({ email, username, password: hashedPassword });
+        await client.close();
+
+        res.status(201).send("User created successfully");
+    } catch (error) {
+        console.error(error);
+        await client.close();
+        res.status(500).send("Error in saving user");
+    }
 });
 
 // Test route
@@ -22,6 +75,6 @@ app.get('/test', (req, res) => {
     res.send('Test route is working!');
 });
 
-// Listening on port 3000
-const PORT = 3000;
+// Listening on port 5000
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
